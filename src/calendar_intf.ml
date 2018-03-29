0e553c47550a604f8620952218778769fb450ab3
Yunfeng Lin <yunfeng.lin04@gmail.com>
2018-03-29 14:52:52 +1100

    move is_holiday to module Calendar

--- src/calendar_intf.ml
+++ src/calendar_intf.ml
@@ -4,12 +4,14 @@ module type S =
 sig
   type t 
   val create: unit -> t
-  val is_holiday: t -> Date.t -> bool
 end
 
+
 module type Calendar = sig
+  type t
+  val is_holiday: t -> Date.t -> bool
   module Make (H: Holidayable_intf.S) : S with type t := Date.Hash_set.t
   module US_settlement : S with type t := Date.Hash_set.t
   module US_libor_impact : S with type t := Date.Hash_set.t
   module US_government_bond : S with type t := Date.Hash_set.t
-end
+end with type t := Date.Hash_set.t
