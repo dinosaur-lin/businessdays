e9f16ed50b791f48f8a07285ac3d60e623f049cd
Yunfeng Lin <yunfeng.lin04@gmail.com>
2018-03-31 02:21:04 +1100

    add calendar adjust

--- src/calendar_intf.ml
+++ src/calendar_intf.ml
@@ -9,9 +9,15 @@ end
 
 module type Calendar = sig
   type t
+
   val is_holiday: t -> Date.t -> bool
+
+  (** Adjusts a non-business day to the appropriate near business day with respect to the given convention. *)
+  val adjust: t -> Date.t -> Business_day_convention.t -> Date.t
+
   module Make (H: Holidayable_intf.S) : S with type t := Date.Hash_set.t
   module US_settlement : S with type t := Date.Hash_set.t
   module US_libor_impact : S with type t := Date.Hash_set.t
   module US_government_bond : S with type t := Date.Hash_set.t
+
 end with type t := Date.Hash_set.t
