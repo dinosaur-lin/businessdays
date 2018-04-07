13242a84c9b7d11f43445eb91b43320617d8a0ae
Yunfeng Lin <yunfeng.lin04@gmail.com>
2018-04-08 06:18:26 +1000

    add name to calendar since business_252 needs it

--- src/calendar_intf.ml
+++ src/calendar_intf.ml
@@ -2,22 +2,28 @@ open! Core_kernel
 
 module type S = 
 sig
-  type t 
+  type t
+  val name: string
   val create: unit -> t
 end
 
+module type N =
+sig
+  val name: string
+end
 
 module type Calendar = sig
-  type t
+  type t = { cache: Date.Hash_set.t;
+             name: string; }
 
   val is_holiday: t -> Date.t -> bool
 
   (** Adjusts a non-business day to the appropriate near business day with respect to the given convention. *)
   val adjust: t -> Date.t -> Business_day_convention.t -> Date.t
 
-  module Make (H: Holidayable_intf.S) : S with type t := Date.Hash_set.t
-  module US_settlement : S with type t := Date.Hash_set.t
-  module US_libor_impact : S with type t := Date.Hash_set.t
-  module US_government_bond : S with type t := Date.Hash_set.t
+  module Make (H: Holidayable_intf.S)(N: N) : S with type t := t 
+  module US_settlement : (S with type t := t)
+  module US_libor_impact : (S with type t := t)
+  module US_government_bond : (S with type t := t)
 
-end with type t := Date.Hash_set.t
+end
