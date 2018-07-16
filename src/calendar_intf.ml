70af5b62225739e089bf7b811c31064d2337e9a7
Yunfeng Lin <yunfeng.lin04@gmail.com>
2018-07-16 23:29:20 +0000

    Add some comments

--- src/calendar_intf.ml
+++ src/calendar_intf.ml
@@ -19,20 +19,23 @@ module type Calendar = sig
              name: string; }
 
   val name: t -> string
-
-  (** number of days for this month in a year *)
+  
   val month_days: int -> Month.t -> int
+  (** [month_days year month] number of days for this month [month] in a year [year] *)
 
   val is_end_of_month: Date.t -> bool
+  (** [is_end_of_month dt] determine whether a date [dt] is the end of month *)
 
-  (** whether a date is holiday or not *)
   val is_holiday: t -> Date.t -> bool
+  (** [is_holiday cal dt] determine whether a date [dt] is holiday or not in [cal] *)
 
-  (** Adjusts a non-business day to the appropriate near business day with respect to the given convention. *)
   val adjust: t -> Date.t -> Business_day_convention.t -> Date.t
+  (** [adjust cal dt bdc] Adjusts a non-business day [dt] to the appropriate nearest 
+  business day with respect to the given convention [bdc], according to calendar [cal] *)
 
-  (** calculate the number of business days between two days *)
   val business_days_between: t -> Date.t -> Date.t -> int
+  (** [business_days_between cal dt1 dt2] calculate the number of business days between two 
+  days [dt1] and [dt2] *)
 
   module Make (H: Holidayable_intf.S)(N: N) : S with type t := t 
   module US_settlement : (S with type t := t)
