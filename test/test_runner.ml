31958b29ac2060a1106ab7e82cd49aa965b37f0d
Yunfeng Lin <yunfeng.lin04@gmail.com>
2018-07-24 03:52:39 +0000

    add test for simple day counter

--- test/test_runner.ml
+++ test/test_runner.ml
@@ -3,4 +3,5 @@ let () =
     "US holidays", Unit_us_holidays.test_set;
     "US calendars", Unit_calendar.test_set;
     "Day counter thirty 360", Unit_thirty_360.test_set;
+    "Simple day counter", Unit_simple_day_counter.test_set
   ]
---
+++ test/unit_simple_day_counter.ml
@@ -0,0 +1,25 @@
+open! Core_kernel
+open Businessdays
+
+module To_test = struct
+  
+  let test_simple_day_counter () =    
+    let periods = [ 3; 6; 12 ] (* months *) in 
+    let first = Date.create_exn ~y:2002 ~m:Jan ~d:1 in 
+    let ends = List.map periods ~f:(fun p -> Date.add_months first p) in 
+    List.map ends ~f:(fun e -> Simple_day_counter.year_frac first e)
+end
+
+let test_simple_day_counter () =
+  Alcotest.(check (list (float 1e-12))) "test simple day counter" [0.25; 0.5; 1.0] (To_test.test_simple_day_counter ())
+
+let test_set = [
+  "test_simple_day_counter", `Slow, test_simple_day_counter
+]
+
+
+
+
+
+
+
