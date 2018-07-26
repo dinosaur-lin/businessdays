7348b9d7effdac0df29c30ac9bf068b6a711ae0c
Yunfeng Lin <yunfeng.lin04@gmail.com>
2018-07-26 05:23:34 +0000

    one more test case

--- test/unit_business_252.ml
+++ test/unit_business_252.ml
@@ -9,6 +9,7 @@ module To_test = struct
     let test_dates = [ 
         c 2002 Feb 1,c 2002 Feb 4; 
         c 2002 Feb 4,c 2003 May 16;
+        c 2003 May 16,c 2003 Dec 17
     ]
     in 
     let module Dc = (val (Business_252.create Calendar.us_settlement): Day_counter_intf.S) in 
@@ -29,7 +30,7 @@ let test_business_252_day_count () =
 
 let test_business_252_year_frac () =
   Alcotest.(check (list (float 1e-5))) "test simple day counter" [0.003968253968253968; 
-  1.2817460317460319] (To_test.test_business_252_year_frac ())
+  1.2817460317460319;0.5833333333333334] (To_test.test_business_252_year_frac ())
 
 let test_set = [
   "test_busienss_252_day_count", `Slow, test_business_252_day_count;
